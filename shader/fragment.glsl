8dcfd7f89b8cefbc69a6301b5f8ac7ffe8710588
Paul Frische <paul@paulfrische.xyz>
2022-10-15 12:07:09 +0200

    added basic light

--- shader/fragment.glsl
+++ shader/fragment.glsl
@@ -1,10 +1,18 @@
 #version 330 core
 
 in vec3 Color;
+in vec3 Normal;
+in vec3 Position;
 
 out vec4 FragColor;
 
 void main()
 {
-    FragColor = vec4(Color, 1.0f);
+    vec3 light_position = vec3(-30.0f, 50.0f, 30.0f);
+    float ambient_light = 0.75f;
+
+    float angle = dot(Normal, normalize(light_position - Position));
+    float dynamic_light = (1.0f - ambient_light) * angle;
+    dynamic_light = max(0, dynamic_light);
+    FragColor = vec4(Color * (ambient_light * dynamic_light), 1.0f);
 }
--- shader/vertex.glsl
+++ shader/vertex.glsl
@@ -2,14 +2,19 @@
 
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec3 aNormal;
 
 uniform mat4 projection;
 uniform mat4 view;
 
 out vec3 Color;
+out vec3 Normal;
+out vec3 Position;
 
 void main()
 {
     Color = aColor;
+    Normal = aNormal;
+    Position = aPos;
     gl_Position = projection * view * vec4(aPos, 1.0f);
 }
